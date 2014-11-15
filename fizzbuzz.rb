7fcae0916f98d529e9a0ddf86141fca27cf0c579
GS888 <gsnydermail@gmail.com>
2014-11-15 17:05:39 -0500

    coding challenges

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+nums = Array(1..100)
+nums.each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "fizz buzz"
+  elsif num % 3 == 0
+    puts "fizz"
+  elsif num % 5 == 0
+    puts "buzz"
+  else
+    puts num
+  end
+end
